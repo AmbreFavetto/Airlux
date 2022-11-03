63442f2cda53e0e77fb06beae8ba568269c80c21
valentin houdu <valentinhoudu@MacBook-Pro-de-valentin-2.local>
2022-11-03 16:28:34 +0100

    ajout fichier val

---
+++ src/db_locale/scribeFile.js
@@ -0,0 +1,19 @@
+import redis from 'redis';
+
+const client = redis.createClient({
+    url: 'redis://admin:pass@redislocale:6379/'
+})
+
+await client.connect()
+
+
+
+
+
+
+
+
+
+
+
+client.quit()
