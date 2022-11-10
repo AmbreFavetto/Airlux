aae116b3f14db678e3e3fba92960f1fcecd95b94
Valentin <valentin.hpro1@gmail.com>
2022-11-10 14:59:22 +0100

    add controller

---
+++ src/db_distante-node/app/controllers/sensors.controller.js
@@ -0,0 +1,92 @@
+import database from '../db.config.js';
+import Response from '../domain/response.js';
+import logger from '../util/logger.js'; // ?????????
+import QUERY from '../query/sensor.query.js';
+
+const HttpStatus = {
+  OK: { code: 200, status: 'OK' },
+  CREATED: { code: 201, status: 'CREATED' },
+  NO_CONTENT: { code: 204, status: 'NO_CONTENT' },
+  BAD_REQUEST: { code: 400, status: 'BAD_REQUEST' },
+  NOT_FOUND: { code: 404, status: 'NOT_FOUND' },
+  INTERNAL_SERVER_ERROR: { code: 500, status: 'INTERNAL_SERVER_ERROR' }
+};
+
+export const getActuators = (req, res) => {
+  logger.info(`${req.method} ${req.originalUrl}, fetching actuators`);
+  database.query(QUERY.SELECT_ACTUATORS, (error, results) => {
+    if (!results) {
+      res.status(HttpStatus.OK.code)
+        .send(new Response(HttpStatus.OK.code, HttpStatus.OK.status, `No actuator found`));
+    } else {
+      res.status(HttpStatus.OK.code)
+        .send(new Response(HttpStatus.OK.code, HttpStatus.OK.status, `Actuators retrieved`, { actuators: results }));
+    }
+  });
+};
+
+export const createActuator = (req, res) => {
+  logger.info(`${req.method} ${req.originalUrl}, creating actuator`);
+  database.query(QUERY.CREATE_ACTUATOR_PROCEDURE, Object.values(req.body), (error, results) => {
+    if (!results) {
+      logger.error(error.message);
+      res.status(HttpStatus.INTERNAL_SERVER_ERROR.code)
+        .send(new Response(HttpStatus.INTERNAL_SERVER_ERROR.code, HttpStatus.INTERNAL_SERVER_ERROR.status, `Error occurred`));
+    } else {
+      //const patient = { id: results.insertedId, ...req.body, created_at: new Date() };
+      const sensor = results[0][0];
+      res.status(HttpStatus.CREATED.code)
+        .send(new Response(HttpStatus.CREATED.code, HttpStatus.CREATED.status, `Actuator created`, { sensor }));
+    }
+  });
+};
+
+export const getActuator = (req, res) => {
+  logger.info(`${req.method} ${req.originalUrl}, fetching actuator`);
+  database.query(QUERY.SELECT_ACTUATOR, [req.params.id], (error, results) => {
+    if (!results[0]) {
+      res.status(HttpStatus.NOT_FOUND.code)
+        .send(new Response(HttpStatus.NOT_FOUND.code, HttpStatus.NOT_FOUND.status, `Actuator by id ${req.params.id} was not found`));
+    } else {
+      res.status(HttpStatus.OK.code)
+        .send(new Response(HttpStatus.OK.code, HttpStatus.OK.status, `Actuator retrieved`, results[0]));
+    }
+  });
+};
+
+export const updateActuator = (req, res) => {
+  logger.info(`${req.method} ${req.originalUrl}, fetching actuator`);
+  database.query(QUERY.SELECT_ACTUATOR, [req.params.id], (error, results) => {
+    if (!results[0]) {
+      res.status(HttpStatus.NOT_FOUND.code)
+        .send(new Response(HttpStatus.NOT_FOUND.code, HttpStatus.NOT_FOUND.status, `Actuator by id ${req.params.id} was not found`));
+    } else {
+      logger.info(`${req.method} ${req.originalUrl}, updating actuator`);
+      database.query(QUERY.UPDATE_ACTUATOR, [...Object.values(req.body), req.params.id], (error, results) => {
+        if (!error) {
+          res.status(HttpStatus.OK.code)
+            .send(new Response(HttpStatus.OK.code, HttpStatus.OK.status, `Actuator updated`, { id: req.params.id, ...req.body }));
+        } else {
+          logger.error(error.message);
+          res.status(HttpStatus.INTERNAL_SERVER_ERROR.code)
+            .send(new Response(HttpStatus.INTERNAL_SERVER_ERROR.code, HttpStatus.INTERNAL_SERVER_ERROR.status, `Error occurred`));
+        }
+      });
+    }
+  });
+};
+
+export const deleteActuator = (req, res) => {
+  logger.info(`${req.method} ${req.originalUrl}, deleting Actuator`);
+  database.query(QUERY.DELETE_ACTUATOR, [req.params.id], (error, results) => {
+    if (results.affectedRows > 0) {
+      res.status(HttpStatus.OK.code)
+        .send(new Response(HttpStatus.OK.code, HttpStatus.OK.status, `Actuator deleted`, results[0]));
+    } else {
+      res.status(HttpStatus.NOT_FOUND.code)
+        .send(new Response(HttpStatus.NOT_FOUND.code, HttpStatus.NOT_FOUND.status, `Actuator by id ${req.params.id} was not found`));
+    }
+  });
+};
+
+export default HttpStatus;
