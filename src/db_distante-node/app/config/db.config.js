3846a085f10adce961bbccad0b7087e49095f9f6
Valentin <valentin.hpro1@gmail.com>
2022-11-18 11:10:42 +0100

    enfin ça se lance

--- src/db_distante-node/app/config/db.config.js
+++ src/db_distante-node/app/config/db.config.js
@@ -1,6 +1,14 @@
-module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "123",
-    DB: "test"
-  };
+import mysql from 'mysql';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  connectionLimit: process.env.DB_CONNECTION_LIMIT
+});
+
+export default pool;
--- src/db_distante-node/app/index.js
+++ src/db_distante-node/app/index.js
@@ -3,19 +3,57 @@ import ip from 'ip';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import Response from './domain/response.js';
-import HttpStatus from './controllers';
-import routes from './routes';
+import HttpStatusBatiment from './controllers/batiment.controller.js';
+import HttpStatusDevice from './controllers/device.controller.js';
+import HttpStatusFloor from './controllers/floor.controller.js';
+import HttpStatusRoom from './controllers/room.controller.js';
+import HttpStatusUser from './controllers/user.controller.js';
+import routesBatiment from './routes/batiment.routes.js';
+import routesDevice from './routes/device.routes.js';
+import routesFloor from './routes/floor.routes.js';
+import routesRoom from './routes/room.routes.js';
+import routesUser from './routes/user.routes.js';
 import logger from './util/logger.js';
 
+
 dotenv.config();
-const PORT = process.env.SERVER_PORT || 3000;
+const PORT = 8000;
 const app = express();
 app.use(cors({ origin: '*' }));
 app.use(express.json());
 
-app.use('/', routes);
-app.get('/', (req, res) => res.send(new Response(HttpStatus.OK.code, HttpStatus.OK.status, 'Airlux DB distante API, v1.0.0 - All Systems Go')));
-app.all('*', (req, res) => res.status(HttpStatus.NOT_FOUND.code)
-  .send(new Response(HttpStatus.NOT_FOUND.code, HttpStatus.NOT_FOUND.status, 'Route does not exist on the server')));
-app.listen(PORT, () => logger.info(`Server running on: ${ip.address()}:${PORT}`));
+//Batiment
+app.use('/', routesBatiment);
+app.get('/', (req, res) => res.send(new Response(HttpStatusBatiment.OK.code, HttpStatusBatiment.OK.status, 'Airlux DB distante API, v1.0.0 - All Systems Go')));
+app.all('*', (req, res) => res.status(HttpStatusBatiment.NOT_FOUND.code)
+  .send(new Response(HttpStatusBatiment.NOT_FOUND.code, HttpStatusBatiment.NOT_FOUND.status, 'Route does not exist on the server')));
 
+//Device
+app.use('/', routesDevice);
+app.get('/', (req, res) => res.send(new Response(HttpStatusDevice.OK.code, HttpStatusDevice.OK.status, 'Airlux DB distante API, v1.0.0 - All Systems Go')));
+app.all('*', (req, res) => res.status(HttpStatusDevice.NOT_FOUND.code)
+  .send(new Response(HttpStatusDevice.NOT_FOUND.code, HttpStatusDevice.NOT_FOUND.status, 'Route does not exist on the server')));
+
+
+//Floor
+app.use('/', routesFloor);
+app.get('/', (req, res) => res.send(new Response(HttpStatusFloor.OK.code, HttpStatusFloor.OK.status, 'Airlux DB distante API, v1.0.0 - All Systems Go')));
+app.all('*', (req, res) => res.status(HttpStatusFloor.NOT_FOUND.code)
+  .send(new Response(HttpStatusFloor.NOT_FOUND.code, HttpStatusFloor.NOT_FOUND.status, 'Route does not exist on the server')));
+
+
+//Room
+app.use('/', routesRoom);
+app.get('/', (req, res) => res.send(new Response(HttpStatusRoom.OK.code, HttpStatusRoom.OK.status, 'Airlux DB distante API, v1.0.0 - All Systems Go')));
+app.all('*', (req, res) => res.status(HttpStatusRoom.NOT_FOUND.code)
+  .send(new Response(HttpStatusRoom.NOT_FOUND.code, HttpStatusRoom.NOT_FOUND.status, 'Route does not exist on the server')));
+
+
+//User
+app.use('/', routesUser);
+app.get('/', (req, res) => res.send(new Response(HttpStatusUser.OK.code, HttpStatusUser.OK.status, 'Airlux DB distante API, v1.0.0 - All Systems Go')));
+app.all('*', (req, res) => res.status(HttpStatusUser.NOT_FOUND.code)
+  .send(new Response(HttpStatusUser.NOT_FOUND.code, HttpStatusUser.NOT_FOUND.status, 'Route does not exist on the server')));
+
+
+app.listen(PORT, () => logger.info(`Server running on: ${ip.address()}:${PORT}`));
