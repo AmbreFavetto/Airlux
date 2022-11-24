06222c81cdfd50628bf8ae18dea9fab35ee4ac4a
ClaraKrchr <clara.krchr@gmail.com>
2022-11-24 12:05:24 +0100

    CA MARCHE

--- pulsor/src/pulsor/filter.py
+++ pulsor/src/pulsor/filter.py
@@ -1,26 +1,25 @@
 import subprocess
 import time
 import paho.mqtt.client as mqtt
-# import datetime
+import datetime
 
-# print(datetime.datetime.now())
 i = 1
 host = "mqtt"
 port = 1883
-print("ok 1")
 client = mqtt.Client()
-print("ok 2")
 
 client.connect(host, port)
-print("ok 3")
+print("Client connecté")
 
-# une fois que le client sera connecté, il faut envoyer la value return dans le bash + la phrase pour insérer en bdd
-# + l'index i
+client.publish("MESSAGE", "salut")
+print("Publish MESSAGE ok")
 
-# while 1:
-#     value = subprocess.run(["/var/pulsor/pulsor.sh"])
-#     print(value)
-#     # print(datetime.datetime.now())
-#     i = i + 1
-
-#     time.sleep(5)
+while 1:
+    value = subprocess.run(["/var/pulsor/pulsor.sh"])
+    print(value)
+    valueToPublish = ("HSET number:%s value '%s' timestamp '%s'" % (i, value.returncode, datetime.datetime.now()))
+    print(valueToPublish)
+    client.publish("PULSOR", valueToPublish)
+    print("Pulsor published")
+    i = i + 1
+    time.sleep(5)
