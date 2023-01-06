e5ab6aee8db59f75c506b8844b72f2092d1d773e
ClaraKrchr <clara.krchr@gmail.com>
2023-01-06 15:04:48 +0100

    wip : code propre formulaire

--- app_airlux/lib/buildings/addBuilding_page.dart
+++ app_airlux/lib/buildings/addBuilding_page.dart
@@ -1,54 +1,126 @@
 import 'package:flutter/material.dart';
+import '../main.dart';
 
-class AddBuildingPage extends StatelessWidget {
+class AddBuildingPage extends StatefulWidget {
   const AddBuildingPage({super.key});
-  static const String title = 'Ajouter un bâtiment';
-
   @override
-  Widget build(BuildContext context) => Scaffold(
-        // changer le nom avec la classe qui permet la navigation
-        //drawer: NavigationDrawerWidget(),
-        appBar: AppBar(
-          title: const Text(AddBuildingPage.title),
-          centerTitle: true,
-          backgroundColor: const Color(0x009fc5e8),
-        ),
-      );
+  AddBuildingPageState createState() => AddBuildingPageState();
 }
 
-class buildForm extends StatefulWidget {
-  @override
-  _buildForm createState() => _buildForm();
-}
+class AddBuildingPageState extends State<AddBuildingPage> {
+  final _formKey = GlobalKey<FormState>();
 
-class _buildForm extends State<buildForm> {
-  TextEditingController name = TextEditingController();
+  TextEditingController title = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-            alignment: Alignment.centerLeft,
-            decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: const [
-                  BoxShadow(color: Colors.black26, offset: Offset(0, 2))
-                ]),
-            height: 60,
-            child: TextField(
-                controller: name,
-                keyboardType: TextInputType.text,
-                style: TextStyle(color: Colors.black87),
-                decoration: InputDecoration(
-                    border: InputBorder.none,
-                    contentPadding: EdgeInsets.only(top: 14),
-                    prefixIcon: Icon(Icons.business, color: Colors.black),
-                    hintText: 'Nom du bâtiment',
-                    hintStyle: TextStyle(color: Colors.black38)))),
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Ajouter un bâtiment'),
+      ),
+      body: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(width: 10.0, height: 10.0),
+              InputText(name: title, inputTitle: 'Nom du bâtiment'),
+              Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(8),
+                height: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF003b71),
+                      ),
+                      child: const Text('Retour'),
+                      onPressed: () => {
+                        if (title.text.isNotEmpty)
+                          {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => MyApp()),
+                            ),
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text('Le bâtiment a été ajouté.'),
+                              ),
+                            ),
+                          }
+                        else
+                          {
+                            print('pas ok'),
+                          }
+                      },
+                    ),
+                    const SizedBox(width: 10.0, height: 0.0),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF003b71),
+                      ),
+                      child: const Text('Sauvegarder'),
+                      onPressed: () => {
+                        if (title.text.isNotEmpty)
+                          {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => MyApp()),
+                            ),
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text('Le bâtiment a été ajouté.'),
+                              ),
+                            ),
+                          }
+                        else
+                          {
+                            print('pas ok'),
+                          }
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
 
+class InputText extends StatelessWidget {
+  const InputText({
+    Key? key,
+    required this.name,
+    required this.inputTitle,
+  }) : super(key: key);
+
+  final TextEditingController name;
+  final String inputTitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: const EdgeInsets.only(left: 20.0, right: 20.0),
+        alignment: Alignment.centerLeft,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: const [
+              BoxShadow(color: Colors.black26, offset: Offset(0, 2))
+            ]),
+        height: 50,
+        child: TextField(
+            controller: name,
+            keyboardType: TextInputType.emailAddress,
+            style: const TextStyle(color: Colors.black87),
+            textAlign: TextAlign.center,
+            decoration: InputDecoration(
+                border: InputBorder.none,
+                contentPadding: const EdgeInsets.only(top: 14),
+                hintText: inputTitle,
+                hintStyle: const TextStyle(color: Colors.black38))));
+  }
+}
