688f19f942ed0b5cd31647cebbd7e49fc82a839f
ClaraKrchr <clara.krchr@gmail.com>
2023-01-26 11:15:49 +0100

    main à NE PAS COMMIT

--- app_airlux/lib/main.dart
+++ app_airlux/lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
-	class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
