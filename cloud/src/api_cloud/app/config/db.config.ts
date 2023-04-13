9953ac59beef6510f3dec1a7f450e7f76bd95087
ambrefavetto <ambre.favetto@gmail.com>
2023-04-13 09:08:29 +0200

    Modify config for local tests

--- cloud/src/api_cloud/app/config/db.config.ts
+++ cloud/src/api_cloud/app/config/db.config.ts
@@ -5,9 +5,9 @@ dotenv.config();
 
 const pool: mysql.Pool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
-  port: Number(process.env.DB_PORT) || 3306,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT) || 8306,
+  user: process.env.DB_USER || 'admin',
+  password: process.env.DB_PASSWORD || 'letmein',
   database: process.env.DB_NAME || "airlux_cloud_db",
   connectionLimit: Number(process.env.DB_CONNECTION_LIMIT) || 20
 });
