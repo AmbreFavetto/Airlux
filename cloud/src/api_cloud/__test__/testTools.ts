bc90683d4ddded37e09a470428ef9e035cb40b1a
ambrefavetto <ambre.favetto@gmail.com>
2023-04-28 11:43:39 +0200

    test

--- cloud/src/api_cloud/__test__/testTools.ts
+++ cloud/src/api_cloud/__test__/testTools.ts
@@ -1,6 +1,6 @@
 import pool from '../app/config/db.config';
 
-// TEST ci
+// TEST 
 
 const QUERY = {
     CREATE_BUILDING: 'INSERT INTO building(name, building_id) VALUES ("testBuilding", "123")',
