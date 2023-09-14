0447a3fd60480ad6cdbcd3391b3141860ada4489
ambrefavetto <ambre.favetto@gmail.com>
2023-09-14 20:35:31 +0200

    fix some update bugs

--- cloud/src/api_cloud/app/controllers/syncLog.controller.ts
+++ cloud/src/api_cloud/app/controllers/syncLog.controller.ts
@@ -3,13 +3,25 @@ import { Request, Response, NextFunction } from 'express';
 import logger from '../util/logger';
 import HttpStatus from '../util/devTools';
 
+const regex = /"message":"(POST|PUT|DELETE) ([^"]+) ({.*?})"/g;
+
 export const syncLog = async (req: Request, res: Response) => {
   try {
-    // Vérifiez si un fichier a été téléchargé avec multer
     if (!req.body.file) {
       return res.status(HttpStatus.BAD_REQUEST.code)
         .send(new ResponseFormat(HttpStatus.BAD_REQUEST.code, HttpStatus.BAD_REQUEST.status, `No file uploaded`));
     }
+    const matches = [...req.body.file.matchAll(regex)];
+    matches.forEach((match) => {
+      const method = match[1];
+      const route = match[2];
+      const body = match[3];
+
+      logger.info(`Méthode : ${method}`);
+      logger.info(`Route : ${route}`);
+      logger.info(`Body : ${body}`);
+      logger.info('---');
+    })
 
     return res.status(HttpStatus.OK.code)
       .send(new ResponseFormat(HttpStatus.OK.code, HttpStatus.OK.status, `File processed successfully`));
