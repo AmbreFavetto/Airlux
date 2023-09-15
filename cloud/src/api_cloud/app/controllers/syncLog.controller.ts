250bae3e80eb2a9f10b66dcff8119f2b59c71e8a
ambrefavetto <ambre.favetto@gmail.com>
2023-09-15 18:34:55 +0200

    synchro

--- cloud/src/api_cloud/app/controllers/syncLog.controller.ts
+++ cloud/src/api_cloud/app/controllers/syncLog.controller.ts
@@ -1,9 +1,10 @@
 import ResponseFormat from '../domain/responseFormat';
 import { Request, Response, NextFunction } from 'express';
-import logger from '../util/logger';
 import HttpStatus from '../util/devTools';
+import axios from 'axios';
 
 const regex = /"message":"(POST|PUT|DELETE) ([^"]+) ({.*?})"/g;
+const routePfx = 'http://api_cloud:3010';
 
 export const syncLog = async (req: Request, res: Response) => {
   try {
@@ -12,15 +13,18 @@ export const syncLog = async (req: Request, res: Response) => {
         .send(new ResponseFormat(HttpStatus.BAD_REQUEST.code, HttpStatus.BAD_REQUEST.status, `No file uploaded`));
     }
     const matches = [...req.body.file.matchAll(regex)];
-    matches.forEach((match) => {
+    matches.forEach(async (match) => {
       const method = match[1];
-      const route = match[2];
-      const body = match[3];
+      const route = routePfx + match[2];
+      let body = match[3].replaceAll("\\", "");
 
-      logger.info(`Méthode : ${method}`);
-      logger.info(`Route : ${route}`);
-      logger.info(`Body : ${body}`);
-      logger.info('---');
+      if (method === "POST") {
+        await axios.post(route, JSON.parse(body));
+      } else if (method === "PUT") {
+        await axios.put(route, JSON.parse(body));
+      } else if (method === "DELETE") {
+        await axios.delete(route, JSON.parse(body));
+      }
     })
 
     return res.status(HttpStatus.OK.code)
