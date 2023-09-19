484d345c595b23f0ee4c0e16307beb8b00e2266e
ambrefavetto <ambre.favetto@gmail.com>
2023-09-19 21:20:17 +0200

    fix health

--- local/src/api_local/app/controllers/health.controller.ts
+++ local/src/api_local/app/controllers/health.controller.ts
@@ -1,8 +1,14 @@
 import ResponseFormat from '../domain/responseFormat';
-import { Response } from 'express';
+import { Response, Request } from 'express';
 import HttpStatus from '../util/devTools';
 
-export const health = async (res: Response) => {
-    return res.status(HttpStatus.OK.code)
-        .send(new ResponseFormat(HttpStatus.OK.code, HttpStatus.OK.status, `Access local-api_ok`));
+export const health = async (req: Request, res: Response) => {
+    try {
+        return res.status(HttpStatus.OK.code)
+            .send(new ResponseFormat(HttpStatus.OK.code, HttpStatus.OK.status, `Access local-api ok`));
+    } catch (err) {
+        return res.status(HttpStatus.INTERNAL_SERVER_ERROR.code)
+            .send(new ResponseFormat(HttpStatus.INTERNAL_SERVER_ERROR.code, HttpStatus.INTERNAL_SERVER_ERROR.status, `Access local-api nok`));
+    }
+
 };
