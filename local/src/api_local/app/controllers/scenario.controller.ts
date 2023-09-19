702d7d4e206c3fa3c7be22bd4539e5eefac7b6b6
ambrefavetto <ambre.favetto@gmail.com>
2023-09-19 20:07:25 +0200

    fix import

--- local/src/api_local/app/controllers/scenario.controller.ts
+++ local/src/api_local/app/controllers/scenario.controller.ts
@@ -6,7 +6,6 @@ import scenarioCreateSchema, { scenarioUpdateSchema } from '../models/scenario.m
 import { v4 as uuidv4 } from 'uuid';
 import HttpStatus, { deleteElt, getRelationToDelete } from '../util/devTools';
 import Scenario from '../interfaces/scenario.interface';
-import { addLog } from '../util/logFile.js';
 import { sendToKafka } from '../config/kafka.config';
 
 function setData(req: Request) {
@@ -31,7 +30,7 @@ export const createScenario = async (req: Request, res: Response) => {
       req.body.scenario_id = uuidv4();
     }
     await database.hmset(`scenarios:${req.body.scenario_id}`, data);
-    if (req.headers.sync && req.headers.sync === "1"){
+    if (req.headers.sync && req.headers.sync === "1") {
       sendToKafka('sendToMysql', "POST /scenario/ " + JSON.stringify(req.body))
     }
     res.status(HttpStatus.CREATED.code)
@@ -95,9 +94,6 @@ export const updateScenario = async (req: Request, res: Response) => {
         .send(new ResponseFormat(HttpStatus.NOT_FOUND.code, HttpStatus.NOT_FOUND.status, `Scenario by id ${req.params.id} was not found`));
     }
     await database.hmset(`scenarios:${req.params.id}`, req.body);
-    if (req.headers.sync && req.headers.sync && req.headers.sync === "1") {
-      addLog("PUT", `/scenario/${req.params.id}`, JSON.stringify(req.body))
-    }
     res.status(HttpStatus.CREATED.code)
       .send(new ResponseFormat(HttpStatus.CREATED.code, HttpStatus.CREATED.status, `Scenario updated`, { id: req.params.id, ...req.body }));
   } catch (error) {
@@ -116,9 +112,6 @@ export const deleteScenario = async (req: Request, res: Response) => {
     }
     await getRelationToDelete("scenarios:" + req.params.id)
     await deleteElt("scenarios:" + req.params.id)
-    if (req.headers.sync && req.headers.sync && req.headers.sync === "1") {
-      addLog("DELETE", `/scenario/${req.params.id}`, JSON.stringify(req.body))
-    }
     res.status(HttpStatus.OK.code)
       .send(new ResponseFormat(HttpStatus.OK.code, HttpStatus.OK.status, `Scenario deleted`));
   } catch (err) {
